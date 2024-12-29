2c285868ace9fc658b665d1cedd3e4662ee35adb
Coder <111493707+informationabout@users.noreply.github.com>
2024-12-29 19:33:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -47,13 +47,6 @@ window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
 
-const myDog = new Dog("Golden Retriever");
-myDog.makeSound();
-myDog.bark();
-
-const myCat = new Cat("Siamese");
-myCat.makeSound();
-myCat.purr();
 
 
 
